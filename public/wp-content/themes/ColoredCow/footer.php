de2d63b7bf775864b648932cfd7c144057fcbee7
VamsiRao <s.vamsirao@gmail.com>
2017-07-10 13:18:01 +0530

    Add Image to the Carousel

--- public/wp-content/themes/ColoredCow/footer.php
+++ public/wp-content/themes/ColoredCow/footer.php
@@ -1,4 +1,7 @@
 	<?php wp_footer(); ?>
 </body>
+<div class="container col-md-12 col-xs-12 footer">
+	
+</div>
 
 </html>
--- public/wp-content/themes/ColoredCow/landing.php
+++ public/wp-content/themes/ColoredCow/landing.php
@@ -7,9 +7,14 @@
     get_header();
 ?>
 <div class="jumbotron jumbotron-inverse jumbotron-hero jumbo-bg">
+
+	<!-- <video id="video_background" preload muted autoplay loop>
+	    <source  src="<?php echo get_template_directory_uri(); ?>/events_video.mp4" type="video/mp4">
+  	</video> -->
+  		
 	<div class="container">
 		<div class="row">
-			<div class="col-md-12 col-xs-12">
+			<div class="col-md-12 col-xs-12">				
 				<div class="col-md-6 col-xs-12">
 		
 					<?php
@@ -54,29 +59,29 @@
 	<div class="row">
 		<div class="col-md-12 col-xs-12">
 						
-			<div class="col-md-6 col-xs-12">
-				<div id="events_corousel" class="corousel slide">
-					<ol class="corousel-indicators">
-						<li data-target="#events_corousel" data-slide-to="0" class="active"></li>
-						<li data-target="events_corousel" data-slide-to="1"></li>
-						<li data-target="events_corousel" data-slide-to="2"></li>
+			<div class="col-md-6 col-xs-12" >
+				<div id="events_carousel" class="carousel slide" data-ride="carousel" style="margin-top: 50px">
+					<ol class="carousel-indicators">
+						<li data-target="#events_carousel" data-slide-to="0" class="active"></li>
+						<li data-target="events_carousel" data-slide-to="1"></li>
+						<li data-target="events_carousel" data-slide-to="2"></li>
 					</ol>
-					<!-- <div class="corousel-inner">
-						<div class="item-active">
-							<img src="soiree1.jpg" alt="1">
+					<div class="carousel-inner">
+						<div class="item active">
+							<img  src="<?php echo get_template_directory_uri(); ?>/soiree1.jpg" alt="1">
 						</div>
 						<div class="item">
-							<img src="soiree2.jpg" alt="2">
+							<img src="<?php echo get_template_directory_uri(); ?>/soiree2.jpg" alt="2">
 						</div>
 						<div class="item">
-							<img src="soiree3.jpg" alt="3">
+							<img src="<?php echo get_template_directory_uri(); ?>/soiree3.jpg" alt="3">
 						</div>
-					</div> -->
-					<a class="left carousel-control" href="#events_corousel" data-slide="prev">
+					</div>
+					<a class="left carousel-control" href="#events_carousel" data-slide="prev">
 						<span class="glyphicon glyphicon-chevron-left"></span>
 						<span class="sr-only">Previous</span>
 					</a>
-					<a class="right carousel-control" href="#events_corousel" data-slide="next">
+					<a class="right carousel-control" href="#events_carousel" data-slide="next">
 					    <span class="glyphicon glyphicon-chevron-right"></span>
 						<span class="sr-only">Next</span>
 					</a>
